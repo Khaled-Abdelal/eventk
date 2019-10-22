18d08dd2b5aa1003c6a283506301ff91859d8b34
Khaled Abdelal <Khaled.Abdelal@outlook.com>
2019-10-22 17:05:37 +0200

    switched from gmaps to leaflet powered by mapbox

--- client/src/Layout.js
+++ client/src/Layout.js
@@ -8,6 +8,12 @@ function Layout({ children }) {
       <Head>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/leaflet@1.5.1/dist/leaflet.css"
+          integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
+          crossOrigin=""
+        />
       </Head>
       <Nav />
       {children}
--- client/src/Map.js
+++ client/src/Map.js
@@ -1,34 +1,52 @@
 import React, { useState, useEffect, useRef } from 'react';
-import GoogleMapReact from 'google-map-react';
 // import usePosition from '../hooks/usePosition';
 import io from 'socket.io-client';
+import { Map as LeafMap, TileLayer, Marker, Popup } from 'react-leaflet';
+import { Button } from '@material-ui/core';
+import L from 'leaflet';
 import Modal from './Modal';
 import Event from './Event';
 import useModal from '../hooks/useModal';
 import EventForm from './EventForm';
-import ClickMarker from './ClickMarker';
 import { BaseURL } from '../constants';
 import { AuthContainer } from '../hooks/useAuth';
+import AddEventMarker from './AddEventMarker';
 
 function Map({ events }) {
   // const { latitude, longitude, error } = usePosition();
   const { open, handleClose, handleOpen } = useModal();
-  // const [mapObjects, setMapObjects] = useState({});
   const { user, token } = AuthContainer.useContainer();
   const [stateEvents, setStateEvents] = useState(events);
   const [cardActiveIndex, setCardActiveIndex] = useState('');
-  const [show, setShow] = useState(false);
   const [coords, setCoords] = useState({ lat: null, lng: null });
   const { current: socket } = useRef(io(BaseURL));
 
-  // const handleApiLoaded = (map, maps) => {
-  //   // use map and maps objects
-  //   setMapObjects({ map, maps });
-  // };
+  const myCustomColour = '#583470';
+
+  const markerHtmlStyles = `
+  background-color: ${myCustomColour};
+  width: 2rem;
+  height: 2rem;
+  display: block;
+  left: -.9rem;
+  top: -.9rem;
+  position: relative;
+  border-radius: 2rem 2rem 0;
+  transform: rotate(45deg);
+  border: 1px solid #FFFFFF`;
+
+  const icon = L.divIcon({
+    className: 'my-custom-pin',
+    iconAnchor: [0, 24],
+    labelAnchor: [-6, 0],
+    popupAnchor: [0, -36],
+    html: `<span style="${markerHtmlStyles}" />`,
+  });
   function handleCardToggle(e) {
+    setCoords({ lat: null, lng: null });
     setCardActiveIndex('');
-    if (cardActiveIndex === e.currentTarget.getAttribute('value')) return;
-    setCardActiveIndex(e.currentTarget.getAttribute('value'));
+    if (cardActiveIndex === e.target._popup.options.children.props.event._id) return;
+    setCardActiveIndex(e.target._popup.options.children.props.event._id);
   }
   useEffect(() => {
     try {
@@ -66,41 +84,54 @@ function Map({ events }) {
     }
   };
 
-  const mapClicked = ({ lat, lng }) => {
+  const mapClicked = e => {
+    setCardActiveIndex('');
+    const { lat, lng } = e.latlng;
     setCoords({ lat, lng });
-    setShow(true);
-    console.log(coords);
   };
   return (
-    <div style={{ height: '100%', width: '100%' }}>
-      <GoogleMapReact
-        // bootstrapURLKeys={{ key: '/* YOUR KEY HERE */' }}
+    <React.Fragment>
+      <LeafMap
+        style={{ width: '100vw', height: 'calc(100vh - 65px)' }}
+        zoom={6}
+        center={[29.924526, 31.205753]}
         onClick={mapClicked}
-        defaultCenter={{
-          lat: 31.205753,
-          lng: 29.924526,
-        }}
-        defaultZoom={6}
-        yesIWantToUseGoogleMapApiInternals
-        // onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
-        style={{ cursor: 'pointer' }}
       >
-        <ClickMarker setShow={setShow} show={show} lat={coords.lat} lng={coords.lng} onClick={handleOpen} />
+        {coords.lat && coords.lng && cardActiveIndex === '' && (
+          <AddEventMarker position={[coords.lat, coords.lng]}>
+            <Popup>
+              <Button className="openButton" size="small" onClick={handleOpen} variant="contained" color="default">
+                Add Event Here
+              </Button>
+            </Popup>
+          </AddEventMarker>
+        )}
+
         {stateEvents.map(event => (
-          <Event
+          <Marker
+            onClick={handleCardToggle}
             key={event._id}
-            event={event}
-            lat={event.location.coordinates[1]}
-            lng={event.location.coordinates[0]}
-            cardActiveIndex={cardActiveIndex}
-            handleCardToggle={handleCardToggle}
-          />
+            position={[event.location.coordinates[1], event.location.coordinates[0]]}
+            icon={icon}
+          >
+            <Popup style={{ padding: 0 }}>
+              <Event event={event} cardActiveIndex={cardActiveIndex} handleCardToggle={handleCardToggle} />
+            </Popup>
+          </Marker>
         ))}
-      </GoogleMapReact>
+        <TileLayer
+          url={`https://api.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.jpg90?access_token=${process.env.MAPBOX_API_KEY}`}
+        />
+      </LeafMap>
       <Modal handleClose={handleClose} handleOpen={handleOpen} open={open}>
         <EventForm onSubmit={uploadEvent} />
       </Modal>
-    </div>
+      <style jsx global>{`
+        .leaflet-popup-content-wrapper {
+          padding: 0px;
+        }
+      `}</style>
+    </React.Fragment>
   );
 }
 
