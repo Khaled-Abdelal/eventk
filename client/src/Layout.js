308bb7040a3a7fcb92927691c386a6acbd065953
Khaled Abdelal <Khaled.Abdelal@outlook.com>
2019-10-26 00:48:49 +0200

    leaflet bug fix

--- client/src/Layout.js
+++ client/src/Layout.js
@@ -4,7 +4,7 @@ import Nav from './Nav';
 
 function Layout({ children }) {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
+    <div style={{ display: 'flex', flexDirection: 'column' }}>
       <Head>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
